2820f1de9a3cf4e92707355a9961e31ca3d8de9d
YassineElkefi <yassine.elkefi6@gmail.com>
2025-09-16 10:31:18 +0100

    Initial Commit

---
+++ Islami/IslamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslamiApp.swift
+//  Islami
+//
+//  Created by Yassine EL KEFI on 16/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct IslamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
